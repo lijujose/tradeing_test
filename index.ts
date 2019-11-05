0a5f97f0faa12620f9a9b1d094315f8d6f320e4d
Liju Jose <liju.jose@gds.ey.com>
2019-11-06 00:05:57 +0530

    unit test run test_data.json file

--- index.ts
+++ index.ts
@@ -1,30 +1,57 @@
-const figure: Array<Array<number>> = []
+let shape: Array<Array<number>> = []
 let iStart:number = 0, jStart:number = 0;
 
-export const draw = function (width: number, height: number, padding: number) {
+/**
+ * This function creates a infinite shapes of a given height and Width and 
+ * recursively create inner shapes of same Height - Padding and Width - Padding until there is no space left in the last shape.
+ * @param  {number} width
+ * @param  {number} height
+ * @param  {number} padding
+ * @return {Array}       final shape
+ */
+export function draw(width: number, height: number, padding: number) {
   // padding has to divisble of 2
   if (padding % 2 !== 0) throw Error("Not a valid padding");
 
   //stop recursive call, if no more shape is possible
-  if ((width - jStart) < 3 || (height - iStart) < 3) return figure;
+  if ((width - jStart) <= 1 || (height - iStart) <= 1) return shape;
 
   for(let i = iStart; i < height; i++) {
-    figure[i] = figure[i] || [];
+    shape[i] = shape[i] || [];
     for(let j = jStart; j < width; j++) {
-      if (i === iStart || i === (height - 1)) figure[i][j] = 1;
-      else if (j === jStart || j ===(width - 1)) figure[i][j] = 2;
-      else figure[i][j] = 0
+      if (i === iStart || i === (height - 1)) shape[i][j] = 1;
+      else if (j === jStart || j ===(width - 1)) shape[i][j] = 2;
+      else shape[i][j] = 0
     }
   }
 
+  const diff: number = (padding/2) + 1;
+
   //staring point of next shape
-  iStart += padding/2;
-  jStart += padding/2;
+  iStart += diff;
+  jStart += diff;
 
   //recursive call
-  return draw(width - (padding/2), height - (padding/2), padding);
+  return draw(width - (diff), height - (diff), padding);
 }
 
-export const unset = function() {
+/**
+ * This function clear iStart and jStart value so new shape can formed
+ */
+export function unset() {
   iStart = 0; jStart = 0;
+  shape = [];
+}
+
+/**
+ * This function format and log 2-Dimensional array(final shape) in matrix from
+ */
+export function print2DShape() {
+  for(let arr of shape) {
+    let line: string = "";
+    for(let val of arr) {
+      line += `${val} `;
+    }
+    console.log(line);
+  }
 }
--- test.js
+++ test.js
@@ -1,29 +1,36 @@
 const assert = require('assert');
 const _module = require("./index");
+const testData = require("./test_data.json");
 
 describe("Test draw function", () => {
   afterEach(() => {
     _module.unset();
   });
 
-  it("should not create shape width=2, height=3", () => {
-    const shape = _module.draw(2,3,2);
-    assert.equal(shape.length, 0);
-    console.log(shape);
+  it("should create last possible square shape: width=2, height=2, padding=2", () => {
+    const shape = _module.draw(2,2,2);
+    const pixelString = shape.join();
+    assert.equal(pixelString.indexOf("2"), -1);
+    assert.equal(pixelString.indexOf("0"), -1);
+    _module.print2DShape();
   });
 
-  it("should create last possible square shape: width-3, height=3", () => {
-    const shape = _module.draw(3,3,2);
+  it("should create last possible sqaure shape with smallest possible padding(one recursion): width=6, height=6, padding=", () => {
+    const shape = _module.draw(6,6,2);
     assert.equal(shape[1][1], 0);
-    console.log(shape);
+    assert.equal(shape[2][2], 1);
+    assert.equal(shape[3][3], 1);
+    _module.print2DShape();
   });
 
-  it("should create last possible sqaure shape with smallest possible padding(one recursion): width=5, height=5, padding=2", () => {
-    const shape = _module.draw(5,5,2);
-    assert.equal(shape[2][2], 0);
-    assert.equal(shape[2][3], 2);
-    assert.equal(shape[1][3], 1);
-    console.log(shape);
+  it("should create last possible rectangle shape (width > height) : width=3, height=2, padding=2", () => {
+    const shape = _module.draw(3,2,2);
+    _module.print2DShape();
+  });
+
+  it("should create last possible rectangle shape (width < height) : width=2, height=3, padding=2", () => {
+    const shape = _module.draw(2,3,2);
+    _module.print2DShape();
   });
 
   it("should throw error: Not a valid padding", () => {
@@ -36,13 +43,20 @@ describe("Test draw function", () => {
   });
 
   it("should create shapes: width=20, height=20, padding=4", () => {
-    const shape = _module.draw(20,20,4);
-    assert.equal(shape[0][0], 1);
-    assert.equal(shape[1][1], 0);
-    assert.equal(shape[2][2], 1);
+    const shape = _module.draw(60,60,10);
+    _module.print2DShape();
+  });
 
-    assert.equal(shape[9][9], 0);
-    assert.equal(shape[10][10], 0);
-    console.log(shape);
-  })
+  it("test with 'test_data.json' file", () => {
+    for(let data of testData) {
+      _module.unset();
+      const args = data.input.split(",");
+      const shape = _module.draw(
+        Number(args[0]),
+        Number(args[1]),
+        Number(args[2]),
+      );
+      assert.equal(JSON.stringify(shape), data.pixelArrayJson);
+    }
+  });
 });
